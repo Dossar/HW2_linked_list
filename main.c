af0fa43e0bfa7f802ace277f73f5f7d445f8409d
unknown <Michael_Meding@os314-05.fs.uml.edu>
2013-09-16 14:15:20 -0400

    added all the files -MM

---
+++ main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: Michael_Meding
+ *
+ * Created on September 16, 2013, 2:11 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    printf("holy crap it works");
+    return (EXIT_SUCCESS);
+}
+
